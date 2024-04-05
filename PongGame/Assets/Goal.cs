8b9580144587e200ec4cfdeb79320f8b8563581b
josephineangie <josephine.angie@ti.ukdw.ac.id>
2024-04-06 00:47:42 +0700

    pong game assets commit

---
+++ PongGame/Assets/Goal.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool isPlayerGoal;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            if (!isPlayerGoal)
+            {
+                Debug.Log("Player 1 Scored...");
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player1Scored();
+            }
+            else
+            {
+                Debug.Log("Player 2 Scored...");
+                GameObject.Find("GameManager").GetComponent<GameManager>().Player2Scored();
+            }
+        }
+    }
+}
